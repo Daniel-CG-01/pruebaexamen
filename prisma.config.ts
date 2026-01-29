14089256227e1363531ee95628c9965c94a491df
DanielZR05 <danielzanon2005@gmail.com>
2026-01-29 12:03:28 +0100

    En proceso de crear una base de datos PostgreSQL independiente utilizando Docker Compose

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,18 +4,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-
   schema: "prisma/schema.prisma",
-
-  // migrations: {
-  //   path: "prisma/migrations",
-  // },
-
-  // datasource: {
-  //   url: process.env["DATABASE_URL"],
-  // },
-
-  migrate: {
-    datasourceUrl: process.env.DATABASE_URL,
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
   }
 });
